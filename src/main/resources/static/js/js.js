2a9665da563e939b7d740241176774a28606b029
Fiarr4ik <fiarr4ik@gmail.com>
2024-06-10 09:05:00 +0700

    fix problems

--- src/main/resources/static/js/js.js
+++ src/main/resources/static/js/js.js
@@ -1,5 +1,5 @@
 let score = 0;
-const milestones = [101, 251, 501, 751, 1001, 1501, 2001, 3001, 5001, 7501, 10001, 15001, 20001, 25001, 50001];
+const milestones = [100, 250, 500, 750, 1000, 1500, 2000, 3001, 5001, 7500, 10001, 15000, 20000, 25000, 50000,500000];
 let currentMilestoneIndex = 0;
 let scorerank = 1;
 
@@ -26,50 +26,54 @@ function updateProgressBar() {
     progressPercentageElement.textContent = Math.floor(progressPercentage) + '%';
     clicksLeftElement.textContent = `${milestone - score} собранных скуфов до следующего уровня`;
 
-    if (score < milestones[0]) {
+    if (score <= milestones[0]) {
         rankElement.textContent = "Новичок Скуфозавоев";
-    } else if (score < milestones[1]) {
+    } else if (score <= milestones[1]) {
         rankElement.textContent = "Охотник за Скуфами";
         scorerank = 2;
-    } else if (score < milestones[2]) {
+    } else if (score <= milestones[2]) {
         rankElement.textContent = "Эксперт по Скуфам";
         scorerank = 3;
-    } else if (score < milestones[3]) {
+    } else if (score <= milestones[3]) {
         rankElement.textContent = "Собиратель Скуфов";
         scorerank = 4;
-    } else if (score < milestones[4]) {
+    } else if (score <= milestones[4]) {
         rankElement.textContent = "Проворный Скуфолов";
         scorerank = 5;
-    } else if (score < milestones[5]) {
+    } else if (score <= milestones[5]) {
         rankElement.textContent = "Мастер Скуфопада";
         scorerank = 6;
-    } else if (score < milestones[6]) {
+    } else if (score <= milestones[6]) {
         rankElement.textContent = "Скуфомант";
         scorerank = 7;
-    } else if (score < milestones[7]) {
+    } else if (score <= milestones[7]) {
         rankElement.textContent = "Герой Скуфов";
         scorerank = 8;
-    } else if (score < milestones[8]) {
+    } else if (score <= milestones[8]) {
         rankElement.textContent = "Легенда Скуфов";
         scorerank = 9;
-    } else if (score < milestones[9]) {
+    } else if (score <= milestones[9]) {
         rankElement.textContent = "Чемпион Скуфов";
         scorerank = 10;
-    } else if (score < milestones[10]) {
+    } else if (score <= milestones[10]) {
         rankElement.textContent = "Магистр Скуфов";
         scorerank = 11;
-    } else if (score < milestones[11]) {
+    } else if (score <= milestones[11]) {
         rankElement.textContent = "Император Скуфов";
         scorerank = 12;
-    } else if (score < milestones[12]) {
+    } else if (score <= milestones[12]) {
         rankElement.textContent = "Властелин Скуфов";
         scorerank = 13;
-    } else if (score < milestones[13]) {
+    } else if (score <= milestones[13]) {
         rankElement.textContent = "Король Скуфов";
         scorerank = 14;
-    } else {
+    } else if (score <= milestones[14]){
         rankElement.textContent = "Бог Скуфов";
         scorerank = 15;
+    }
+    else if (score >= milestones[15]) {
+        rankElement.textContent = "Чел иди поспи скуфы тебе не рады(";
+        scorerank = 150;
     }
 
     if (score >= milestone) {
