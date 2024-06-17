393b4dde1edd153ebf2edb0ca33fc139bd4acf71
Fiarr4ik <fiarr4ik@gmail.com>
2024-06-17 20:54:37 +0700

    fix: Controller mapping

--- src/main/java/ru/fiarr4ik/skufwars/Controllers/MainController.java
+++ src/main/java/ru/fiarr4ik/skufwars/Controllers/MainController.java
@@ -1,12 +1,25 @@
 package ru.fiarr4ik.skufwars.Controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller
-public class MainController {
-    @GetMapping("/main")
-    public String main(){
-        return "main";
+    @Controller
+    @RequestMapping("/SkufWars")
+    public class MainController {
+
+        @RequestMapping("/")
+        public String mainPage() {
+            return "main";
+        }
+
+        @RequestMapping("/login")
+        public String loginPage() {
+            return "login";
+        }
+
+        @RequestMapping("/register")
+        public String registerPage() {
+            return "register";
+        }
+
     }
-}
